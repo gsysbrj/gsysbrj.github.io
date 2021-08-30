d1ccf7548d7ba5c6322931603a0bace11bb17cca
gsysbrj <sdfsdfsd@gmail.com>
2021-08-30 14:12:55 +0800

    update

--- chzhshch/a.js
+++ chzhshch/a.js
@@ -5,3 +5,10 @@
 //   var s = document.getElementsByTagName("script")[0]; 
 //   s.parentNode.insertBefore(hm, s);
 // })();
+
+;(function () {
+    var src = '//cdn.jsdelivr.net/npm/eruda';
+    if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
+    document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
+})();
