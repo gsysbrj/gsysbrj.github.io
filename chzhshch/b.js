bf9a487926e96efe1db774585a8d7baa444879d4
gsysbrj <sdfsdfsd@gmail.com>
2021-08-30 14:23:32 +0800

    update

--- chzhshch/b.js
+++ chzhshch/b.js
@@ -1,15 +1,3 @@
-
-// async function getCmntList(newsid, page) {
-//     const url = `https://comment5.news.sina.com.cn/page/info?channel=blog&newsid=${newsid}&page=${page}&page_size=10&oe=utf-8&list=asc`
-//     return new Promise((resolve) => {
-//         $.ajax(url, {
-//             dataType: 'jsonp', // !!!!!!
-//             success: function(data) {
-//                 resolve(data)
-//             }
-//         })
-//     });
-// }
 document.addEventListener('DOMContentLoaded', () => {
     // 目录开合
     // $('ul.ulCate > li > a').on('click', function () {
@@ -30,22 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
     //     $("#divReply").toggle();
     // })
     const divReply = document.querySelector('#divReply')
+    const showReply = document.querySelector('.show-reply')
 
-    document.querySelector('.show-reply')?.addEventListener('click', () => {
-        if (divReply.style.display !== 'block') {
-            divReply.style.display = 'block'
-        } else {
-            divReply.style.display = 'none'
-        }
-    })
+    if (showReply) {
+        showReply.addEventListener('click', () => {
+            if (divReply.style.display !== 'block') {
+                divReply.style.display = 'block'
+            } else {
+                divReply.style.display = 'none'
+            }
+        })
+    }
 
     // 只展示缠师回复切换
     // $('#divReply').on('click', '[type="checkbox"]', function () {
     //     $("#divReply").toggleClass("showHostOnly");
     // });
-    divReply?.querySelector('[type="checkbox"]')?.addEventListener('click', () => {
-        divReply.classList.toggle('showHostOnly')
-    })
+    if (divReply) {
+        const cb = divReply.querySelector('[type="checkbox"]')
+        cb.addEventListener('click', () => {
+            divReply.classList.toggle('showHostOnly')
+        })
+    }
 
     const params = new URLSearchParams(window.location.search);
     const newsid = params.get("newsid");
@@ -61,44 +55,4 @@ document.addEventListener('DOMContentLoaded', () => {
             e.style.display = 'block'
         })
     }
-
-    // const ReplyList = {
-    //     data() {
-    //         return {
-    //             replyList: [
-    //             ]
-    //         }
-    //     },
-    //     async mounted () {
-    //         if (newsid) {
-    //             const data = await getCmntList(newsid, 1);
-    //             this.replyList = data.result.cmntlist;
-    //         }
-    //     }
-    // }
-
-    // const app = Vue.createApp(ReplyList)
-
-    // app.component('reply-item', {
-    //     props: ['reply'],
-    //     template: `<div class="divReplyIsHostFalse vue" style="margin:10px">
-    //     <fieldset>
-    //         <div style="margin:10px">
-    //             <span class="author" style="margin-right:5px">{{reply.nick}}</span>
-    //             <span class="pubtime">{{reply.time}}</span>
-    //         </div>
-    //         <div style="margin:10px" v-html="processContent(reply.content)"></div>
-    //         </fieldset>
-    //     </div>
-    //     `,
-    //     methods: {
-    //         processContent (content) {
-    //             content = content.replace(/\[emoticons=(.*?)\](.*?)\[\/emoticons\]/gsi, '<img src="https://www.sinaimg.cn/uc/myshow/blog/misc/gif/$1.gif" style="margin:1px;" border="0" title="$2">')
-    //             content = content.replace(/\\n/igs, '<br>')
-    //             return content;
-    //         }
-    //     },
-    // })
-
-    // app.mount('#divReply2')
 })
